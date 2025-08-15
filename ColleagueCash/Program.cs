1b4565bc2011cede855f4547fdf8440b496b9b2c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-15 16:25:50 +0200

    adjust filepath

--- ColleagueCash/Program.cs
+++ ColleagueCash/Program.cs
@@ -5,6 +5,15 @@ class Program
 {
     public static void Main(String[] args)
     {
+        string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        string filePath = Path.Combine(baseDirectory, "WorkLoad\\loan-registration.csv");
+
+        //2T
+        //string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        //string fileInvestments = Path.Combine(baseDirectory, "Csv\\InvestmentsT.csv");
+
+        Console.WriteLine("hier:" + filePath);
+
         string fileName = string.Empty;
 
         IRepositoryLoan repositoryLoan = new RepositoryLoan(fileName);
