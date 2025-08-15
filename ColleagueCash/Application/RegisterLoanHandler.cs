4a9252080777d17e7bcd00975e09145dd773e8bc
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-15 15:53:47 +0200

    add ctor to class

--- ColleagueCash/Application/RegisterLoanHandler.cs
+++ ColleagueCash/Application/RegisterLoanHandler.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ColleagueCash.Domain;
 
 namespace ColleagueCash.Application
 {
     public class RegisterLoanHandler
     {
+        private IRepositoryLoan _repositoryLoan;
+
+        public RegisterLoanHandler(IRepositoryLoan repositoryLoan)
+        {
+            _repositoryLoan = repositoryLoan;
+        }
     }
 }
