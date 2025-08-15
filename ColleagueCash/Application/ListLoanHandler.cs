70958fa3e80da183e0f546e2e70dd356d785a03b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-15 16:33:58 +0200

    implement ctor to ListLoanHandler

---
+++ ColleagueCash/Application/ListLoanHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColleagueCash.Domain;
+
+namespace ColleagueCash.Application
+{
+    public class ListLoanHandler
+    {
+        private IRepositoryLoan _repositoryLoan;
+
+        public ListLoanHandler(IRepositoryLoan repositoryLoan)
+        {
+            _repositoryLoan = repositoryLoan;
+        }
+
+    }
+}
