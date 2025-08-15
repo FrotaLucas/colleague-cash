5d53f553e126b9e7c11758891197b3ab2e0d3ea7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-15 15:32:02 +0200

    implement interface into RepositoryLoan

---
+++ ColleagueCash/Infrastructure/RepositoryLoan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColleagueCash.Domain;
+
+namespace ColleagueCash.Infrastructure
+{
+    public class RepositoryLoan : Interface1
+
+    {
+        public void Add(string name, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Loan> ListLoans()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
