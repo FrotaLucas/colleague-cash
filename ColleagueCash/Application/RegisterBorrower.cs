af7a10378b510a60b12449bef1b7c31e786f18c0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-17 21:57:13 +0200

    create a class RegisterBorrower

---
+++ ColleagueCash/Application/RegisterBorrower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColleagueCash.Domain;
+using ColleagueCash.Infrastructure;
+
+namespace ColleagueCash.Application
+{
+    public class RegisterBorrower
+    {
+
+        public readonly IRepositoryBorrower RepositoryBorrower;
+
+        public RegisterBorrower(IRepositoryBorrower repositoryBorrower)
+        {
+            RepositoryBorrower = repositoryBorrower;
+        }
+
+
+
+    }
+}
