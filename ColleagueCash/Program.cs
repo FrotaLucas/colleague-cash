8a79e990bcc2142a86479c97a5db6d384bcb8f73
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-17 21:00:59 +0200

    create new path to files

--- ColleagueCash/Program.cs
+++ ColleagueCash/Program.cs
@@ -6,20 +6,28 @@ class Program
 {
     public static void Main(String[] args)
     {
+        //COMO ESCONDER ESSA PARTE DA APLICACAO? COLOCAR TUDO DENTRO DE UM app.config ??
         string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
         string fileName = Path.Combine(baseDirectory, "WorkLoad\\loan-registration.csv");
 
-        //2T
-        //string baseDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
-        //string fileInvestments = Path.Combine(baseDirectory, "Csv\\InvestmentsT.csv");
 
-        Console.WriteLine("hier:" + fileName);
+        string borrowerPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        string borrowerFile = Path.Combine(baseDirectory, "WorkLoad\\loan-registration.csv");
+
 
         IRepositoryLoan repositoryLoan = new RepositoryLoan(fileName);
 
         var listLoanHandler = new ListLoanHandler(repositoryLoan);
         var registerLoanHandler = new RegisterLoanHandler(repositoryLoan);
 
-        listLoanHandler.Execute();
+        //listLoanHandler.Execute();
+
+        Loan loan = new Loan
+        {
+            Description = "Sushi",
+            Amount = 100,   
+        };
+
+        registerLoanHandler.Execute(loan);
     }
 }
