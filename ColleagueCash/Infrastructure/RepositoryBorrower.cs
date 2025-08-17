400931758b4e8520d4e3bbfc5b5d450510e1107c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-17 20:55:48 +0200

    implement interface

---
+++ ColleagueCash/Infrastructure/RepositoryBorrower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColleagueCash.Domain;
+
+namespace ColleagueCash.Infrastructure
+{
+    public class RepositoryBorrower : IBorrowerRepository
+    {
+        public void AddNewBorrower(Borrower borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Borrower> GetAllBorrowers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
