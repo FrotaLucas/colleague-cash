b3f7b8e0fcfca2a1cb0d03facbdc6d2abef8050b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-18 13:12:02 +0200

    clean code

--- ColleagueCash/Application/BorrowerService.cs
+++ ColleagueCash/Application/BorrowerService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ColleagueCash.Domain;
-using ColleagueCash.Infrastructure;
+﻿using ColleagueCash.Domain;
 
 namespace ColleagueCash.Application
 {
