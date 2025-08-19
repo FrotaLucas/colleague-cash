52244760f7211801a7d31f488b7a517ca4513593
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-20 00:28:16 +0200

    properties

--- ColleagueCash/Infrastructure/FileSettings.cs
+++ ColleagueCash/Infrastructure/FileSettings.cs
@@ -6,15 +6,11 @@ using System.Threading.Tasks;
 
 namespace ColleagueCash.Infrastructure
 {
-    namespace ColleagueCash.Infrastructure
+    public class FileSettings
     {
-        public class FileSettings
-        {
-            public string LoanFile { get; set; }
-            public string BorrowerFile { get; set; }
-            public string LastBorrowerIdFile { get; set; }
-            public string LastLoanIdFile { get; set; }
-        }
+        public string LoanFile { get; set; }
+        public string BorrowerFile { get; set; }
+        public string LastBorrowerIdFile { get; set; }
+        public string LastLoanIdFile { get; set; }
     }
-
 }
