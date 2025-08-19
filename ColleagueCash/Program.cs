088972560fd13bb62a4fcea43e141de2e66300b2
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-19 19:47:54 +0200

    refactor program.cs

--- ColleagueCash/Program.cs
+++ ColleagueCash/Program.cs
@@ -1,4 +1,5 @@
-﻿using ColleagueCash.Application;
+﻿using System.Net.Sockets;
+using ColleagueCash.Application;
 using ColleagueCash.Domain;
 using ColleagueCash.Infrastructure;
 
@@ -18,51 +19,116 @@ class Program
 
         //last id borrower
         string borrowerIdPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
-        string borrowerSize = Path.Combine(borrowerIdPath, "WorkLoad\\last-borrowerid.txt");
+        string borrowerSize = Path.Combine(borrowerIdPath, "WorkLoad\\last-borrowerId.txt");
 
         //last id borrower
         string loanIdPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
         string loanSize = Path.Combine(loanIdPath, "WorkLoad\\last-loanId.txt");
 
 
-        //BorrowerService
-
-
-        Borrower borrower = new Borrower
-        {
-            Name = "Nathan",
-            FamilyName = "Otta",
-
-        };
-
         IRepositoryBorrower repositoryBorrower = new RepositoryBorrower(borrowerFile, borrowerSize);
+        var borrowerService = new BorrowerService(repositoryBorrower);
 
-        BorrowerService borrowerService = new BorrowerService(repositoryBorrower);
-
-        //borrowerService.AddNewBorrower(borrower);
-
-        //var borrowers = borrowerService.GetAllBorrowersByName();
-        //foreach (var item in borrowers)
-        //{
-        //    Console.WriteLine(item.Name);
-        //    Console.WriteLine(item.FamilyName);
-        //    Console.WriteLine(item.Cellphone);
-        //}
-
-        //Loan 
         IRepositoryLoan repositoryLoan = new RepositoryLoan(loanFile, loanSize, repositoryBorrower);
-
         var loanService = new LoanService(repositoryLoan);
 
-        //loanService.Execute();
-
-        Loan loan = new Loan
+        while (true)
         {
-            Description = "Sushi",
-            Amount = 100,   
-        };
+            Console.WriteLine("\n=== Loan Tracker ===");
+            Console.WriteLine("1 - Register new Loan");
+            Console.WriteLine("2 - Settling a colleague debt");
+            Console.WriteLine("3 - List all debts"); //By amount
+            Console.WriteLine("4 - List all debts ordered by date");
+            Console.WriteLine("5 - List all debts of a colleague");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Your option: ");
 
-        loanService.RegisterNewLoan(loan, borrower);
+            var choice = Console.ReadLine();
+
+
+            string name = string.Empty;
+            string familyName = string.Empty;
+            decimal amount = 0;
+            string description = string.Empty;
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Name and Family name of the Colleague. (ex. Thomas Müller): ");
+                    var fullName1 = Console.ReadLine().Split(' ');
+
+                    name = fullName1[0];
+                    familyName = fullName1[1];
+
+                    //string? cellphone = null;
+                    //Console.WriteLine("Cellphone Number: Y/N:");
+                    //{
+                    //    Console.Write("Enter cellphone number: ");
+                    //    cellphone = Console.ReadLine();
+                    //}
+
+                    Console.Write("Amount: ");
+                    if (decimal.TryParse(Console.ReadLine(), out amount))
+                    {
+                        Console.Write("Short description for the loan: ");
+                        description = Console.ReadLine();
+                     
+                        loanService.RegisterNewLoan(amount, description, name, familyName );
+                        Console.WriteLine("✅ Loan registered!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid Amount");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Name and Family name of the Colleague. (ex. Thomas Müller): ");
+                    var fullName = Console.ReadLine().Split(' ');
+
+                    name = fullName[0];
+                    familyName = fullName[1];
+
+                    Console.Write("Amount to be paid: ");
+                    if (decimal.TryParse(Console.ReadLine(), out amount))
+                    {
+                        loanService.ReduceLoan(name, familyName, amount);
+                        Console.WriteLine("✅ Payment registered!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid Amount");
+                    }
+                    break;
+
+                case "3":
+                    loanService.DisplayAllLoansByAmount();
+                    break;
+
+                case "4":
+                    loanService.DisplayAllLoansByDate();
+                    break;
+
+                case "5":
+                    Console.Write("Name and Family name of the Colleague. (ex. Thomas Müller): ");
+                    var fullName5 = Console.ReadLine().Split(' ');
+
+                    name = fullName5[0];
+                    familyName = fullName5[1];
+
+                    loanService.DisplayAllLoanByName(name, familyName); 
+                    break;
+
+                case "0":
+                    return;
+
+                default:
+                    Console.WriteLine("wrong option.");
+                    break;
+            }
+        }
 
     }
 }
