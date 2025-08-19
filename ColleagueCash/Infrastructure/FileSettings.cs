eefca260c4a120514d9904a18052404c89df2fba
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-19 22:08:16 +0200

    create a new class

---
+++ ColleagueCash/Infrastructure/FileSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColleagueCash.Infrastructure
+{
+    namespace ColleagueCash.Infrastructure
+    {
+        public class FileSettings
+        {
+            public string LoanFile { get; set; }
+            public string BorrowerFile { get; set; }
+            public string LastBorrowerIdFile { get; set; }
+            public string LastLoanIdFile { get; set; }
+        }
+    }
+
+}
