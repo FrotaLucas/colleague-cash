1b3aa24d01634821439127a84504977c9d44fc3a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-21 18:38:42 +0200

    update

--- ColleagueCash/Application/Configuration/AppConfig.cs
+++ ColleagueCash/Application/Configuration/AppConfig.cs
@@ -5,9 +5,10 @@ public class AppConfig
     public DataFilesCSVConfig DataFilesCSV { get; set; } = new();
     public sealed class DataFilesCSVConfig
     {
-        public string loanPath { get; init; }
-        public string borrowerPath { get; init; }
-        public string borrowerIdPath { get; init; }
-        public string loanIdPath { get; init; }
+        public string LoanPath { get; init; }
+        public string BorrowerPath { get; init; }
+        public string LastBorrowerIdFile { get; init; }
+        public string LastLoanIdFile { get; init; }
     }
+
 }
