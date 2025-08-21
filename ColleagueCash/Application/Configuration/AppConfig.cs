ca48a0b602668398c2dcc8c0ab1b97b731b1581e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-21 19:46:55 +0200

    change prop  init  to set

--- ColleagueCash/Application/Configuration/AppConfig.cs
+++ ColleagueCash/Application/Configuration/AppConfig.cs
@@ -5,10 +5,10 @@ public class AppConfig
     public DataFilesCSVConfig DataFilesCSV { get; set; } = new();
     public sealed class DataFilesCSVConfig
     {
-        public string LoanPath { get; init; }
-        public string BorrowerPath { get; init; }
-        public string LastBorrowerIdFile { get; init; }
-        public string LastLoanIdFile { get; init; }
+        public string LoanPath { get; set; }
+        public string BorrowerPath { get; set; }
+        public string LastBorrowerIdFile { get; set; }
+        public string LastLoanIdFile { get; set; }
     }
 
 }
