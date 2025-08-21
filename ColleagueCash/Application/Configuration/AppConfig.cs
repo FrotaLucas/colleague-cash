3052cc7c6e53d45c4018fb2ded239a041cc0aad2
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-21 11:12:14 +0200

    new file to configuration

---
+++ ColleagueCash/Application/Configuration/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace CollegueCashV2.Application.Configuration;
+
+public class AppConfig
+{
+    public DataFilesCSVConfig DataFilesCSV { get; set; } = new();
+    public sealed class DataFilesCSVConfig
+    {
+        public string loanPath { get; init; }
+        public string borrowerPath { get; init; }
+        public string borrowerIdPath { get; init; }
+        public string loanIdPath { get; init; }
+    }
+}
