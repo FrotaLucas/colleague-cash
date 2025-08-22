7b30f3d73c08fa98683d360f3dc359d4e7461a9d
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-22 13:24:13 +0200

    create ILines

---
+++ ColleagueCash/Domain/Contracts/Interfaces/IFiles/IFile.cs
@@ -0,0 +1,15 @@
+﻿using ColleagueCash.Domain.Entities;
+
+namespace ColleagueCash.Domain.Contracts.Interfaces.IFiles
+{
+    public interface IFile
+    {
+        bool Exists(string path);
+        string ReadAllText(string path);
+        void WriteAllText(string path, string content);
+        void AppendAllText(string path, string content);
+        List<Borrower> ReadAllLinesBorrowers(string path);
+
+        List<Loan> ReadAllLinesLoan(string path);
+    }
+}
