f4f310fdf002b9f456726dcbaec5ca7322f56c8b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-22 13:27:40 +0200

    implement IFile into new File class

---
+++ ColleagueCash/Infrastructure/Files/File.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColleagueCash.Domain.Contracts.Interfaces.IFiles;
+using ColleagueCash.Domain.Entities;
+
+namespace ColleagueCash.Infrastructure.Files
+{
+    public class File : IFile
+    {
+        public void AppendAllText(string path, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Borrower> ReadAllLinesBorrowers(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Loan> ReadAllLinesLoan(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadAllText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllText(string path, string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
