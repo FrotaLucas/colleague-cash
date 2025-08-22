68313910ec9ce601870bb8264e38b2773f0273cc
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-22 13:27:53 +0200

    clean unused packages

--- ColleagueCash/Infrastructure/Files/File.cs
+++ ColleagueCash/Infrastructure/Files/File.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ColleagueCash.Domain.Contracts.Interfaces.IFiles;
+﻿using ColleagueCash.Domain.Contracts.Interfaces.IFiles;
 using ColleagueCash.Domain.Entities;
 
 namespace ColleagueCash.Infrastructure.Files
