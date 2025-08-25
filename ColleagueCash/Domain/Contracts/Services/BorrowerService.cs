c77281b4a8e903be872596d63c5ad31d466d6d71
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-25 13:22:53 +0200

    create new method

--- ColleagueCash/Domain/Contracts/Services/BorrowerService.cs
+++ ColleagueCash/Domain/Contracts/Services/BorrowerService.cs
@@ -1,4 +1,5 @@
-﻿using ColleagueCash.Domain.Contracts.Interfaces.IRepository;
+﻿using ColleagueCash.Application.Configuration;
+using ColleagueCash.Domain.Contracts.Interfaces.IRepository;
 using ColleagueCash.Domain.Contracts.Interfaces.IService;
 using ColleagueCash.Domain.Entities;
 
@@ -6,12 +7,15 @@ namespace ColleagueCash.Domain.Contracts.Services
 {
     public class BorrowerService : IBorrowerService
     {
-
         public readonly IBorrowerRepository _repositoryBorrower;
 
-        public BorrowerService(IBorrowerRepository repositoryBorrower)
+        public readonly ILoanRepository _loanRepository;
+
+        public BorrowerService(IBorrowerRepository repositoryBorrower, ILoanRepository loanRepository)
         {
             _repositoryBorrower = repositoryBorrower;
+
+            _loanRepository = loanRepository;
         }
 
         public int AddNewBorrower(string name, string familyName, int? cellphone)
@@ -46,5 +50,23 @@ namespace ColleagueCash.Domain.Contracts.Services
             return;
         }
 
+        public void GetAllBorrowersWithLoans()
+        {
+            var borrowers = _repositoryBorrower.GetAllBorrowers();
+
+            foreach (Borrower b in borrowers)
+            {
+                Console.WriteLine($"Name: {b.Name} {b.FamilyName}\n");
+                b.Loans = _loanRepository.GetAllLoansByBorrowerId(b.BorrowerId);
+
+                b.Loans.Where(loan => loan.Amount > 0)
+                    .ToList()
+                    .ForEach(loan => Console.WriteLine(
+                    $"Outstanding amount: {loan.Amount} - " +
+                    $"Description: {loan.Description} - " +
+                    $"Date of registration: {loan.LoanDate}"
+                ));
+            }
+        }
     }
 }
