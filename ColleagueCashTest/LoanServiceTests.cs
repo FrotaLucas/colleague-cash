6924ddeeb7d72c922d912bbfa04d6a111e1c70cb
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-25 21:14:53 +0200

    delete packages

--- ColleagueCashTest/LoanServiceTests.cs
+++ ColleagueCashTest/LoanServiceTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ColleagueCash.Domain.Contracts.Interfaces.IRepository;
+﻿using ColleagueCash.Domain.Contracts.Interfaces.IRepository;
 using ColleagueCash.Domain.Contracts.Interfaces.IService;
 using ColleagueCash.Domain.Contracts.Services;
 using ColleagueCash.Domain.Entities;
