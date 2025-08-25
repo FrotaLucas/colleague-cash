52a730d5a15ca8045135edfa690ce70c3bf9398d
FrotaLucas <lucasdiasfrota@gmail.com>
2025-08-25 13:23:13 +0200

    register new method

--- ColleagueCash/Domain/Contracts/Interfaces/IService/IBorrowerService.cs
+++ ColleagueCash/Domain/Contracts/Interfaces/IService/IBorrowerService.cs
@@ -5,6 +5,9 @@ namespace ColleagueCash.Domain.Contracts.Interfaces.IService
     public interface IBorrowerService
     {
         public void GetAllBorrowersOrderedByName();
+
+        public void GetAllBorrowersWithLoans();
+
         public int AddNewBorrower(string name, string familyName, int? cellphone);
     }
 }
